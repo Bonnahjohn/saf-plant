7cb2fccbfd41561cd77e4e9c83e8b58e60dcfa94
bonnah joohn <bonnahpoku@gmail.com>
2022-12-07 00:17:55 +0000

    navBar styling for the small screens is modified

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ burger.addEventListener("click", () => {
   burger.classList.toggle("toggle");
   links.classList.toggle("navLinks");
 });
+
+// navbar pick bg-color
+window.addEventListener("scroll", () => {
+  const navbar = document.querySelector(".navs");
+  navbar.classList.toggle("change", scrollY > 0);
+});
